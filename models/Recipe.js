0eb1faa02d3099182fdda5955991d89818a0d69f
Jerryyzhu <jerrybzhu@gmail.com>
2020-02-13 20:19:14 +1100

    Recipe Model

---
+++ models/Recipe.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const RecipeSchema = new mongoose.Schema({
+  title: String,
+  author: String,
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user"
+  },
+  date: { type: Date, default: Date.now },
+  images:[{url: String, description: String}],
+  meta:{
+      favs: Number,
+      votes: Number,
+  },
+  // Minutes
+  prepTime: Number,
+  utensils: [ String ],
+  tags: [String],
+  instructions: [ { step: Number, description: String, image: String}],
+  category: [String],
+  public: Boolean,
+  notes: String
+});
+
+// You can add helper functions to your documents
+module.exports = user = mongoose.model('recipe', RecipeSchema);
