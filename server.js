86f9d4f6ba6644d772266e8252e100096726ad05
Jerryyzhu <jerrybzhu@gmail.com>
2020-02-13 16:53:40 +1100

    mongodb config + other packages

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const connectDB = require('./config/db');
 
 const app = express();
+const port = 8000;
 
-app.get('/', (req, res) => res.send('Hello World'));
+app.get("/", (req, res) => res.send("Hello World"));
+app.listen(port, () => console.log(`FoodPal listening on port ${port}!`));
